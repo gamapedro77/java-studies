d226638c6a7aec2685942794574b470813d08e65
PedrocaMarmota <pedrolucasdgm@gmail.com>
2022-01-31 00:31:02 -0300

    variables

---
+++ src/java_basics/Variables.java
@@ -0,0 +1,14 @@
+package java_basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        // integers declaration and assignment
+        int x = 4;
+        System.out.println(x);
+
+        // string declaration and assignment
+        String hello = "Hello World!";
+        System.out.println(hello);
+
+    }
+}
