3ae4df1f3be3308281b71d412839bec8999f8c45
PedrocaMarmota <pedrolucasdgm@gmail.com>
2022-02-02 08:39:29 -0300

    creating and using methods

---
+++ src/java_basics/Methods.java
@@ -0,0 +1,8 @@
+package java_basics;
+
+public class Methods {
+    public static int sumTwoNumbers(int first, int second) {
+        int sum = first + second;
+        return sum;
+    }
+}
--- src/java_basics/Variables.java
+++ src/java_basics/Variables.java
@@ -2,6 +2,7 @@ package java_basics;
 
 public class Variables {
     public static void main(String[] args) {
+
         // integers declaration and assignment
         int x = 4;
         System.out.println(x);
@@ -10,5 +11,13 @@ public class Variables {
         String hello = "Hello World!";
         System.out.println(hello);
 
+        // arrays declaration and assignment
+        int [] values = new int[100];
+        values[0] = 27;
+        System.out.println(values[0]);
+
+        // using methods
+        int result = Methods.sumTwoNumbers(25, 50);
+        System.out.println(result);
     }
 }
